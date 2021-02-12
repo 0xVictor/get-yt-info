567ff5ca829fc944a2a8d7c9f3fde6e49554adff
Joao Victor <j.victor11@live.com>
2021-02-12 12:53:42 -0300

    Return thumbnail

--- src/index.js
+++ src/index.js
@@ -15,15 +15,15 @@ app.get("/youtube2mp4", (req, res) => {
   youtubedl.getInfo(url, (err, info) => {
     if (err) throw err;
 
-    // console.log('id:', info.id);
-    // console.log('title:', info.title);
-    // console.log('url:', info.url);
-    // console.log('thumbnail:', info.thumbnail);
-    // console.log('description:', info.description);
-    // console.log('filename:', info._filename);
-    // console.log('format id:', info.format_id);
+    // console.log("id:", info.id);
+    // console.log("title:", info.title);
+    // console.log("url:", info.url);
+    // console.log("thumbnail:", info.thumbnail);
+    // console.log("description:", info.description);
+    // console.log("filename:", info._filename);
+    // console.log("format id:", info.format_id);
 
-    return res.json({ url: info.url });
+    return res.json({ url: info.url, thumbnail: info.thumbnail: });
   });
 });
 
