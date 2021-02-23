239332509731d9da579db0b112f0fffe52bec2b9
Joao Victor <j.victor11@live.com>
2021-02-23 13:30:38 -0300

    final improves

--- src/index.js
+++ src/index.js
@@ -27,6 +27,6 @@ app.get("/youtube2mp4", (req, res) => {
   });
 });
 
-app.listen(process.env.PORT || 5000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Server listening on port 5000!");
 });
